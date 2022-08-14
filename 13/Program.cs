6f2e82fd0165e2b031a564dc91edb925426f08f3
egor-turaev <egor-turaev@mail.ru>
2022-08-14 21:01:13 +0300

    13 save

---
+++ 13/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+// string? numberString = Console.ReadLine();
+
+// int number = int.Parse(numberString!);
+
+
+// if (number / 100 > 0)
+// {
+//     int firstDigit = number % 10;
+    
+//     int result = firstDigit;
+
+//     Console.WriteLine(result);
+// }
+
+// else
+// {
+//     Console.WriteLine("Третьей цифры нет");
+// }
+
+
+// Console.WriteLine();
+Console.WriteLine("Введите трёхзначное число:  ");
+
+string str  = Console.ReadLine();
+
+Console.WriteLine (str[2]);
