83f1332d72f24c851b7306da8bf4e70ec1c08cad
egor-turaev <egor-turaev@mail.ru>
2022-08-14 20:59:02 +0300

    save

---
+++ 10/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите трёхзначное число:  ");
+string? numberString = Console.ReadLine();
+int number = int.Parse(numberString!);
+
+
+int second = number % 100;
+
+if(number < 1000)
+{
+    number = second / 10;
+    Console.WriteLine( + number);
+}
+else
+{
+    Console.WriteLine("Чило не трёхзначное");
+}
+Console.WriteLine();
